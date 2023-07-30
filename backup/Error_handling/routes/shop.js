9f831bf319ee901e7b31acf6bc57da335c4374ea
강명덕 <68066437+Profile-exe@users.noreply.github.com>
2023-07-30 09:25:39 +0000

    finished Error handling lesson

---
+++ backup/Error_handling/routes/shop.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router
+  .route('/cart')
+  .get(isAuth, shopController.getCart)
+  .post(isAuth, shopController.postCart);
+
+router.post(
+  '/cart-delete-item',
+  isAuth,
+  shopController.postCartDeleteProduct
+);
+
+router.post('/create-order', isAuth, shopController.postOrder);
+
+router.get('/orders', isAuth, shopController.getOrders);
+
+// router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
