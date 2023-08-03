5c273b134261dd07b3280599db73e3341ff81ca9
강명덕 <68066437+Profile-exe@users.noreply.github.com>
2023-08-03 11:46:27 +0000

    finished ajax and payment lesson

---
+++ backup/Ajax_and_Payment/controllers/error.js
@@ -0,0 +1,17 @@
+module.exports = {
+  get404: (req, res, _next) => {
+    res.status(404).render('404', {
+      pageTitle: 'Page Not Found',
+      path: '/404',
+      isAuthenticated: req.session.isLoggedIn,
+    });
+  },
+
+  get500: (req, res, _next) => {
+    res.status(500).render('500', {
+      pageTitle: 'Server Error!',
+      path: '/500',
+      isAuthenticated: req.session.isLoggedIn,
+    });
+  },
+};
